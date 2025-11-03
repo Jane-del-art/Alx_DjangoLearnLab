0b8067ad0e5f673367ecfed74d2ac88d60adbabe
Jane-del-art <jane46818@gmail.com>
2025-11-03 02:06:18 +0100

    Registered Book model with custom admin configuration

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Register the Book model with custom admin options
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # shows these columns in the admin list view
+    search_fields = ('title', 'author')                     # enables search bar for title and author
+    list_filter = ('publication_year',)                     # adds a filter sidebar for publication year
