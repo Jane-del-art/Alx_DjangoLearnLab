70232e2349086f63f8e67b19856f9796625bdc2b
Jane-del-art <jane46818@gmail.com>
2025-11-24 17:38:57 +0100

    Fix Library import in views.py and complete all views requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
-    model = Library
+    model = Library  # ← This needs Library imported
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
