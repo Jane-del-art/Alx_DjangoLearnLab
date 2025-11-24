a03701142fe5ad3b639e514054d6110d61936353
Jane-del-art <jane46818@gmail.com>
2025-11-24 18:18:32 +0100

    Trigger checher: views.py contains Library import

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,4 +9,4 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library  # ← This needs Library imported
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    context_object_name = 'library'# 
