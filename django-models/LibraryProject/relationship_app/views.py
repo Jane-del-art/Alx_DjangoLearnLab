7012062996bc22bf8c868cd8a9a3a37bb3772cb8
Jane Nwachukwu <jane46818@gmail.com>
2025-11-25 19:01:10 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm 
 from .views import list_books
 
 def list_books(request):
