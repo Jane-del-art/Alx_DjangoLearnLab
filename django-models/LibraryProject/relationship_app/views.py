5e504bf5b687000bd1fedae635743e41c400181e
Jane Nwachukwu <jane46818@gmail.com>
2025-11-25 20:14:29 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -47,3 +47,34 @@ def librarian_view(request):
 @user_passes_test(is_member, login_url='/relationship_app/login/')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+# PERMISSION-BASED VIEWS FOR BOOK MANAGEMENT
+@permission_required('relationship_app.can_add_book', login_url='/relationship_app/login/')
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_name = request.POST.get('author')
+        author, created = Author.objects.get_or_create(name=author_name)
+        Book.objects.create(title=title, author=author)
+        return redirect('list_books')
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book', login_url='/relationship_app/login/')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        author_name = request.POST.get('author')
+        author, created = Author.objects.get_or_create(name=author_name)
+        book.author = author
+        book.save()
+        return redirect('list_books')
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book', login_url='/relationship_app/login/')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
