e0cda10e1908706cd9e142e4f8b9617b0c272c6d
Jane Nwachukwu <jane46818@gmail.com>
2025-11-25 20:32:13 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404  # ← ADD get_
 from django.views.generic import DetailView
 from django.contrib.auth import login  
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required  # ← ADD permission_required
+from django.contrib.auth.decorators import permission_required  # ← ADD permission_required
 from .models import Book, Library, UserProfile, Author  # ← ADD Author import
 
 def list_books(request):
