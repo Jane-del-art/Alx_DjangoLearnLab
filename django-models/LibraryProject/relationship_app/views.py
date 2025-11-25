c9123eff4d96ef331f2aaf50387c43f461e32c98
Jane Nwachukwu <jane46818@gmail.com>
2025-11-25 19:10:25 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,17 +14,18 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-    def login_view(request):
+    
+   def register_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.get_user()
+            user = form.save()
             login(request, user)
             return redirect('list_books')
     else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
-
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+       
 def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
