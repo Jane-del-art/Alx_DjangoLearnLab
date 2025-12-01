d7c91501bf2d95827d864d3d4aa750ba3feb631d
Jane Nwachukwu <jane46818@gmail.com>
2025-12-01 18:28:10 +0100

    Add CustomUser admin and update Book admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,54 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser, Book
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {
+            'fields': (
+                'first_name', 
+                'last_name', 
+                'date_of_birth', 
+                'profile_photo'
+            )
+        }),
+        (_('Permissions'), {
+            'fields': (
+                'is_active', 
+                'is_staff', 
+                'is_superuser', 
+                'groups', 
+                'user_permissions'
+            )
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 
+                'password1', 
+                'password2',
+                'first_name',
+                'last_name',
+                'date_of_birth',
+                'profile_photo'
+            ),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
 
 # Register the Book model with custom admin options
 @admin.register(Book)
@@ -7,3 +56,4 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # shows these columns in the admin list view
     search_fields = ('title', 'author')                     # enables search bar for title and author
     list_filter = ('publication_year',)                     # adds a filter sidebar for publication year
+
