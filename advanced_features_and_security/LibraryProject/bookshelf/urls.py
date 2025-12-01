df57b2746b6831b64dd202348d9bc684f68daf9f
Jane Nwachukwu <jane46818@gmail.com>
2025-12-01 23:23:24 +0100

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', views.create_book, name='create_book'),
     path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('search/', views.secure_search, name='search'),
 ]
