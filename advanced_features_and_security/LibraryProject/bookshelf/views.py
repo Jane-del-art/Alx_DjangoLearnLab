c1ee0feb0876cb76b041e0a4b9eff9c142021e33
Jane Nwachukwu <jane46818@gmail.com>
2025-12-01 18:45:52 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from .models import Book
 
-# Create your views here.
+# View for listing books - requires can_view_book permission
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# View for creating books - requires can_create_book permission
+@permission_required('bookshelf.can_create_book', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
+        Book.objects.create(
+            title=title,
+            author=author,
+            publication_year=publication_year
+        )
+        return redirect('book_list')
+    return render(request, 'bookshelf/create_book.html')
+
+# View for editing books - requires can_edit_book permission
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+# View for deleting books - requires can_delete_book permission
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+# Class-based view with permission checking
+class BookDetailView(PermissionRequiredMixin, DetailView):
+    model = Book
+    template_name = 'bookshelf/book_detail.html'
+    permission_required = 'bookshelf.can_view_book'
