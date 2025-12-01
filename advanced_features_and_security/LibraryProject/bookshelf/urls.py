20c0a3479d61ceb5693ace5f00db9d4470f0d112
Jane Nwachukwu <jane46818@gmail.com>
2025-12-01 18:48:47 +0100

    Add URL patterns for bookshelf app

---
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'bookshelf'
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('create/', views.create_book, name='create_book'),
+    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+]
