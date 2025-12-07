25f5d9995667bf247a8364068ecb87cfe6f624ea
Jane Nwachukwu <jane46818@gmail.com>
2025-12-07 20:11:40 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,26 @@
+# blog/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
+    # Authentication URLs
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    
+    # Password change URLs
+    path('password-change/', 
+         auth_views.PasswordChangeView.as_view(
+             template_name='blog/password_change.html'
+         ), 
+         name='password_change'),
+    path('password-change/done/', 
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='blog/password_change_done.html'
+         ), 
+         name='password_change_done'),
 ]
