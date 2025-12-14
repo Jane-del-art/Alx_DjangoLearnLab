f1c6ce8b1c975ec42d248e42a8937faa5718e045
Jane Nwachukwu <jane46818@gmail.com>
2025-12-14 15:30:36 +0100

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
      'posts',
+    'notifications',
     'accounts',
 ]
 
@@ -143,4 +144,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
 
