c2461ce341d7df6c6a968dc722be2524061d845c
Jane Nwachukwu <jane46818@gmail.com>
2025-12-15 01:00:46 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,7 +8,7 @@ from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 from notifications.models import Notification
-from django.contrib.contenttypes.models import ContentType
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -22,8 +22,9 @@ class PostViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['post'])
     def like(self, request, pk=None):
-        post = generics.get_object_or_404(Post, pk=pk)  # checker wants this
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
+
         if created:
             Notification.objects.create(
                 recipient=post.author,
@@ -32,12 +33,34 @@ class PostViewSet(viewsets.ModelViewSet):
                 target=post
             )
             return Response({'status': 'post liked'}, status=status.HTTP_201_CREATED)
+
         return Response({'status': 'already liked'}, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['post'])
     def unlike(self, request, pk=None):
-        post = generics.get_object_or_404(Post, pk=pk)  # checker wants this
+        post = generics.get_object_or_404(Post, pk=pk)
         deleted, _ = Like.objects.filter(user=request.user, post=post).delete()
+
         if deleted:
             return Response({'status': 'post unliked'}, status=status.HTTP_200_OK)
+
         return Response({'status': 'not liked yet'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
